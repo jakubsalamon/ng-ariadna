abda92a3aae651cab44413d65d39e00358633895
spliteat-corp <spliteat.corp@gmail.com>
2020-01-12 11:06:59 +0100

    added initial page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-ariadna';
+export class AppComponent implements OnInit {
+
+  backgroundOnInit = 'init-bcg-on';
+  logoTextFirst = 'logo__text-first-init';
+  logoTextSecond = 'logo__text-sec-init';
+  logoTextThird = 'logo__text-third-init';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.backgroundOnInit = 'init-bcg-off';
+      this.logoTextFirst = 'logo__text-first';
+      this.logoTextSecond = 'logo__text-sec';
+    }, 400);
+    setTimeout(() => this.logoTextThird = 'logo__text-third', 3000);
+  }
+
+  exitInitPageHandler() {
+    setTimeout(() => {
+      this.backgroundOnInit = 'init-bcg-out';
+    }, 1000);
+  }
 }
